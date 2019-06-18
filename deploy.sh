8b895c65189babae4279ff35f6900f51d84c195d
David Westerink <davidakachaos@gmail.com>
2019-06-18 11:43:18 +0200

    Adjusted deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 # Deploy script
 echo 'Generating tags...'
 ./tag-generator.py
-if git diff-index --quiet HEAD --; then
+if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
     echo 'No new tags generated or old removed.'
 else
     echo 'New tags generated or old removed, adding changes to git'
     git add tag/*
     git commit -m 'Added new tags'
 fi
+echo 'Deploying to GitHub'
+git push
