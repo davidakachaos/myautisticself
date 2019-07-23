da17f3b526a7ef89758dc9e3b11ee0fd28ac8e7c
David Westerink <davidakachaos@gmail.com>
2019-07-23 09:46:00 +0200

    Markup compressed and scaling images

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ let currentSearchTerm = '';
 onload = function () {
    var e = document.getElementById('search-input');
    e.oninput = changeHandler;
-   e.onpropertychange = e.oninput; // for IE8
+   e.onpropertychange = e.oninput;
     document.getElementById('search-input').addEventListener('keyup', handleKeyPress);
 };
 
