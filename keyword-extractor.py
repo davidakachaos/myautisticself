5a87eb7612565c10a45daa27f35e4ca502cc6d4d
David Westerink <davidakachaos@gmail.com>
2019-09-02 14:30:48 +0200

    Ignore tool

--- keyword-extractor.py
+++ keyword-extractor.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
 from multi_rake import Rake
+from bs4 import BeautifulSoup
+import markdown
+import glob
+import subprocess
+
+post_dir = "_posts/"
+filenames = glob.glob(post_dir + "*md")
+print(f"Scanning...{len(filenames)}")
+for filename in filenames:
+    print(f"Processing {filename}")
+    with open(filename, "r", encoding="utf8") as f:
+        content = f.read().split("---")[-1]
+        html = subprocess.run(["kramdown", content], capture_output=True).stdout
+        soup = BeautifulSoup(html, features="html.parser")
+        print(soup.get_text())
+        break
