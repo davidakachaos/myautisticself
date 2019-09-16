719536e1fda83f6836f7576507d5ef6c00b7ce80
David Westerink <davidakachaos@gmail.com>
2019-09-16 10:00:26 +0200

    Adjusted deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,15 @@ if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
 	echo 'Git status not clean, aborting deploy!'
 	exit 1
 fi
-exit 0
 echo 'Generating tags...'
 ./tag-generator.py
 if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
-    echo 'No new tags generated or old removed.'
-else
     echo 'New tags generated or old removed, adding changes to git'
     git add tag/*
     git add category/*
     git commit -m 'Added new tags / category'
+else
+    echo 'No new tags generated or old removed.'
 fi
 echo 'Deploying to GitHub'
 git push
