2ee75fe8f6384d3b5a5626a03cf95e427e13c792
David Westerink <davidakachaos@gmail.com>
2019-10-17 10:02:54 +0200

    Revert "Latest version of My Autistic Self"
    This reverts commit 343eaa7288bf49c0d8b9d838b48316c5948b7f87.

--- js/JekyllWebmentionIO.js
+++ js/JekyllWebmentionIO.js
@@ -753,10 +753,11 @@ exports.Liquid=Liquid;}// #@ts-check
   var targets = [],
       $redirects = document.querySelector('meta[property="webmention:redirected_from"]'),
       redirects,
-      base_url = window.location.origin,
+      base_url = 'https://myautisticself.nl',
       $script;
   
   targets.push( base_url + window.location.pathname );
+  targets.push( base_url.replace('https', 'http') + window.location.pathname );
   if ( $redirects )
   {
     redirects = $redirects.getAttribute('content').split(',');
