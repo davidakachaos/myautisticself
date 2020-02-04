6063efd9ea97836d5dec4c5a04557d8095fd3f03
David Westerink <davidakachaos@gmail.com>
2020-02-04 13:02:04 +0100

    Fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -51,7 +51,8 @@ fi
 echo 'Optimizing site....'
 grunt optimize || exit 1
 echo 'Optimizing images in _site...'
-trimage -q ./_site/assets/img/ || exit 1
+trimage -q -d ./_site/assets/img/ || exit 1
+trimage -q -d ./_site/assets/resized/ || exit 1
 if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
   echo 'Adding optimized images to source...'
   git add assets
