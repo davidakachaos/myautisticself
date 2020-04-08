0341f0186b7e8d4cd38f8f4e66943f10ebef3f0a
David Westerink <davidakachaos@gmail.com>
2020-04-08 11:10:31 +0200

    Optimized images / resized some

--- webready-images.sh
+++ webready-images.sh
@@ -5,14 +5,26 @@ echo 'Converting jpg/jpeg'
 for PHOTO in *.jpg
 do
   BASE=`basename $PHOTO .jpg`
-  convert "$PHOTO" -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpg"
+  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  if [ "$TOO_BIG" -eq "1" ]; then
+    echo "$PHOTO needs a resize..."
+    convert "$PHOTO" -resize 900x900\> -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpg"
+  else
+    convert "$PHOTO" -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpg"
+  fi
   mv webready/$BASE.jpg .
 done
 
 for PHOTO in *.jpeg
 do
   BASE=`basename $PHOTO .jpeg`
-  convert "$PHOTO" -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpeg"
+  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  if [ "$TOO_BIG" -eq "1" ]; then
+    echo "$PHOTO needs a resize..."
+    convert "$PHOTO" -resize 900x900\> -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpeg"
+  else
+    convert "$PHOTO" -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "webready/$BASE.jpeg"
+  fi
   mv webready/$BASE.jpeg .
 done
 
@@ -20,10 +32,20 @@ echo 'Converting png'
 for PHOTO in *.png
 do
   BASE=`basename $PHOTO .png`
-  convert "$PHOTO" -strip "webready/$BASE.png"
+  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  if [ "$TOO_BIG" -eq "1" ]; then
+    echo "$PHOTO needs a resize..."
+    convert "$PHOTO" -resize 900x900\>  -strip "webready/$BASE.png"
+  else
+    convert "$PHOTO" -strip "webready/$BASE.png"
+  fi
   mv webready/$BASE.png .
+  pngquant -f --ext .png --quality 75-80 $PHOTO
 done
 rm -Rf ./webready/
-echo 'Done.'
+echo 'Done converting, optimizing...'
+jpegoptim -q -s --all-progressive *.jpg
+jpegoptim -q -s --all-progressive *.jpeg
+optipng -strip "all" -clobber -silent *.png
 git status .
 cd ../..
