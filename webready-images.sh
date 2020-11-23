c8c8044a632e1f416afbeb6eaa51b54198a3a7aa
David Westerink <davidakachaos@gmail.com>
2020-11-23 12:02:09 +0100

    Improved script

--- webready-images.sh
+++ webready-images.sh
@@ -1,9 +1,9 @@
-cd assets/resized/
+#! /usr/bin/bash
+cd assets/resized/ || exit 1
 echo 'Converting jpg/jpeg'
 for PHOTO in **/*.jpg
 do
-  BASE=`basename $PHOTO .jpg`
-  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  TOO_BIG=$(identify -format "%[fx:w>900&&h>900]" "$PHOTO")
   if [ "$TOO_BIG" -eq "1" ]; then
     echo "$PHOTO needs a resize..."
     convert "$PHOTO" -resize 900x900\> -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "$PHOTO"
@@ -14,8 +14,7 @@ done
 
 for PHOTO in **/*.jpeg
 do
-  BASE=`basename $PHOTO .jpeg`
-  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  TOO_BIG=$(identify -format "%[fx:w>900&&h>900]" "$PHOTO")
   if [ "$TOO_BIG" -eq "1" ]; then
     echo "$PHOTO needs a resize..."
     convert "$PHOTO" -resize 900x900\> -sampling-factor '4:2:0' -strip -quality 85 -interlace JPEG -colorspace sRGB "$PHOTO"
@@ -27,20 +26,18 @@ done
 echo 'Converting png'
 for PHOTO in **/*.png
 do
-  BASE=`basename $PHOTO .png`
-  TOO_BIG=`identify -format "%[fx:w>900&&h>900]" $PHOTO`
+  TOO_BIG=$(identify -format "%[fx:w>900&&h>900]" "$PHOTO")
   if [ "$TOO_BIG" -eq "1" ]; then
     echo "$PHOTO needs a resize..."
     convert "$PHOTO" -resize 900x900\>  -strip "$PHOTO"
   else
     convert "$PHOTO" -strip "$PHOTO"
   fi
-  pngquant -f --ext .png --quality 75-80 $PHOTO
+  pngquant -f --ext .png --quality 75-80 "$PHOTO"
 done
-rm -Rf ./webready/
 echo 'Done converting, optimizing...'
-jpegoptim -q -s --all-progressive **/*.jpg
-jpegoptim -q -s --all-progressive **/*.jpeg
-optipng -strip "all" -clobber -silent **/*.png
+jpegoptim -q -s --all-progressive ./**/*.jpg
+jpegoptim -q -s --all-progressive ./**/*.jpeg
+optipng -strip "all" -clobber -silent ./**/*.png
 git status .
 cd ../..
